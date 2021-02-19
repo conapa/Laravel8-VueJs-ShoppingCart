ed08b28904730625ec5b9519f314726b94d93fad
younessbenssassi <benssassi99@gmail.com>
2021-02-19 09:16:05 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Redirect;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect::route('register');
+});
+
+Route::get('/Products',[ProductController::class,'index'])->name('Products');
+Route::get('/Cart',[CartController::class,'index'])->name('Cart');
+Route::post('/Cart/add',[CartController::class,'AddToCart'])->name('AddToCart');
+Route::post('/Cart/update',[CartController::class,'UpdateFromCart'])->name('UpdateFromCart');
+Route::post('/Cart/remove',[CartController::class,'RemoveFromCart'])->name('RemoveFromCart');
+Route::post('/Cart/coupon',[CartController::class,'UseCoupons'])->name('UseCoupon');
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
